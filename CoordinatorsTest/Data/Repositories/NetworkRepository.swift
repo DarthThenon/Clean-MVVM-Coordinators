0b8b6b54a20ccd332ff670539f963426c0ee8c21
Dmitriy Yurchenko <dmytro.yurchenko1@vivint.com>
2022-02-04 11:52:11 +0200

    decoding error handling, printed response json

--- CoordinatorsTest/Data/Repositories/NetworkRepository.swift
+++ CoordinatorsTest/Data/Repositories/NetworkRepository.swift
@@ -22,8 +22,45 @@ public final class RealNetworkRepository: NetworkRepository {
         
         return session.dataTaskPublisher(for: url)
             .retry(1)
-            .map { $0.data }
+            .map {
+                let data = $0.data
+                
+#if DEBUG
+                print("Request: ", url)
+                print(data.prettyPrintedJSON)
+#endif
+                
+                return data
+            }
             .decode(type: R.self, decoder: decoder)
+            .mapError({ error in
+                
+#if DEBUG
+                if case DecodingError.dataCorrupted(let context) = error {
+                    print(context)
+                } else if case DecodingError.keyNotFound(let key, let context) = error {
+                    print("Key '\(key)' not found:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } else if case DecodingError.valueNotFound(let value, let context) = error {
+                    print("Value '\(value)' not found:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } else if case DecodingError.typeMismatch(let type, let context) = error {
+                    print("Type '\(type)' mismatch:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                }
+#endif
+                
+                return error
+            })
             .eraseToAnyPublisher()
+    }
+}
+private extension Data {
+    var prettyPrintedJSON: NSString {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: .mutableContainers),
+              let jsonData = try? JSONSerialization.data(withJSONObject: object, options: .prettyPrinted)
+        else { return "" }
+        
+        return NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue) ?? ""
     }
 }
