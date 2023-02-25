c6f63436c87f5d1c06cd34b03a6159f3cefb2da8
Dmitriy Yurchenko <yurchenko.d@ideasoft.io>
2023-02-25 23:41:12 +0100

    Updated `Meal` model

--- CoordinatorsTest/Domain/Models/Meal.swift
+++ CoordinatorsTest/Domain/Models/Meal.swift
@@ -8,16 +8,18 @@
 import Foundation
 
 public struct Meal {
-    public let id: String
+    public let id, category: String
     public let imageURL: URL?
     public let title: String
     
     public init(
         id: String,
+        category: String,
         imageURL: URL?,
         title: String
     ) {
         self.id = id
+        self.category = category
         self.imageURL = imageURL
         self.title = title
     }
