d426fc99b56b7648f58d5d8b0cfd9ad82f0023eb
Dmitriy Yurchenko <yurchenko.d@ideasoft.io>
2023-02-25 20:36:08 +0100

    Added `MealsRepositoryTests`

---
+++ CoordinatorsTest/DataTests/Tests/MealsRepositoryTests.swift
@@ -0,0 +1,39 @@
+//
+//  MealsRepositoryTests.swift
+//  DataTests
+//
+//  Created by Dmitriy Yurchenko on 19.02.2023.
+//
+
+import XCTest
+@testable import Data
+
+final class MealsRepositoryTests: XCTestCase {
+    func test() {
+        let (sut, repository) = makeCategoriesSut()
+    }
+}
+
+private extension MealsRepositoryTests {
+    typealias MealCategoryRepository = MockNetworkRepository<MealCategoryContainerCodable>
+    
+    func makeCategoriesSut(file: StaticString = #file,
+                           line: UInt = #line) -> (MealsRepositoryImpl, MealCategoryRepository) {
+        
+        let repository = MealCategoryRepository()
+        let sut = makeSut(networkRepository: repository)
+        
+        return (sut, repository)
+    }
+    
+    func makeSut<T: Decodable>(networkRepository: MockNetworkRepository<T>,
+                               file: StaticString = #file,
+                               line: UInt = #line) -> MealsRepositoryImpl {
+        
+        let sut = MealsRepositoryImpl(networkRepository: networkRepository)
+        
+        deallocationCheck(sut, file: file, line: line)
+        
+        return sut
+    }
+}
