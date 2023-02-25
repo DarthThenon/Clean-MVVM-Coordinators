03489606238aad0967ec284361189ed024995627
Dmitriy Yurchenko <yurchenko.d@ideasoft.io>
2023-02-25 22:54:53 +0100

    Updated `AppDependencyContainer`

--- CoordinatorsTest/CoordinatorsTest/DependencyContainers/AppDependencyContainer.swift
+++ CoordinatorsTest/CoordinatorsTest/DependencyContainers/AppDependencyContainer.swift
@@ -11,16 +11,23 @@ import Domain
 
 final class AppDependencyContainer {
     private let networkRepository: NetworkRepository
+    private let databaseService: DatabaseServiceProtocol
     
     init() {
         networkRepository = RealNetworkRepository()
+        databaseService = DatabaseService()
     }
     
     func createMealDependencyContainer() -> MealDependencyContainer {
-        let mealsRepository = MealsRepositoryImpl(networkRepository: networkRepository)
+        let networkMealsRepository = MealsRepositoryImpl(networkRepository: networkRepository)
+        let localMealsRepository = LocalMealsRepository(databaseService: databaseService)
+        let mealsRepository = CompositeMealsRepository(
+            localRepository: localMealsRepository,
+            networkRepository: networkMealsRepository
+        )
         let getMealCategoriesUseCase = GetMealCategoriesUseCaseImp(mealCategoriesRepository: mealsRepository)
-        let getMealsByCategoryUseCase = GetMealsByCategoryUseCaseImp(mealsByCategoryRepository: mealsRepository)
-        let getMealDetailsByIdUseCase = GetMealDetailsByIdUseCaseImp(repository: mealsRepository)
+        let getMealsByCategoryUseCase = GetMealsByCategoryUseCaseImp(mealsByCategoryRepository: networkMealsRepository)
+        let getMealDetailsByIdUseCase = GetMealDetailsByIdUseCaseImp(repository: networkMealsRepository)
         
         return MealDependencyContainer(getMealsCategoriesUseCase: getMealCategoriesUseCase,
                                        getMealsByCategoryUseCase: getMealsByCategoryUseCase,
