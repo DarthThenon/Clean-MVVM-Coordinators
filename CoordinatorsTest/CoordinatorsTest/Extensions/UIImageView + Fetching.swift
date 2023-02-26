cba255976d42d000df721afb4b4c2624ffb481d5
Dmitriy Yurchenko <yurchenko.d@ideasoft.io>
2023-02-26 14:57:20 +0100

    Refactored image fetching extension

--- CoordinatorsTest/CoordinatorsTest/Extensions/UIImageView + Fetching.swift
+++ CoordinatorsTest/CoordinatorsTest/Extensions/UIImageView + Fetching.swift
@@ -21,19 +21,28 @@ extension UIImageView {
         } else if let image = URLCache().cachedResponse(for: URLRequest(url: url)).flatMap({ UIImage(data: $0.data) }) {
             self.image = image
         } else {
-            return URLSession.shared.dataTaskPublisher(for: url)
-                .retry(1)
-                .compactMap { UIImage(data: $0.data) }
-                .receive(on: OperationQueue.main)
-                .sink(receiveCompletion: { _ in },
-                      receiveValue: { [unowned self] image in
-                    
-                    self.image = image
-                    
-                    cache.setObject(image, forKey: url.absoluteString as NSString)
-                })
+            return imageFetchingPublisher(from: url, cachedIn: cache)
         }
         
         return nil
+    }
+}
+
+private extension UIImageView {
+    func imageFetchingPublisher(
+        from url: URL,
+        cachedIn cache: NSCache<NSString, UIImage>
+    ) -> AnyCancellable {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .retry(1)
+            .compactMap { UIImage(data: $0.data) }
+            .receive(on: OperationQueue.main)
+            .sink(receiveCompletion: { _ in },
+                  receiveValue: { [unowned self] image in
+                
+                self.image = image
+                
+                cache.setObject(image, forKey: url.absoluteString as NSString)
+            })
     }
 }
